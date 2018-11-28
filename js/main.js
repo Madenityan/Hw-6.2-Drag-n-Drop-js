c40ef1c6ab09786c9c25edb0165bf3836938de5a
Madenityan <marinakorsyn@gmail.com>
2018-11-28 22:46:39 +0200

    fixed a functions removeItem and addItem

--- js/main.js
+++ js/main.js
@@ -1,16 +1,18 @@
-let btnRemove = document.getElementById('remove');
-let btnAdd = document.getElementById('add');
 
-function addRemoveButton() {
-    if (btnRemove.style.display !== 'none') {
-        btnRemove.style.display = 'none';
+function addItem(event) {
+    let removeButton = event.target.parentElement.getElementsByClassName('btn-remove')[0];
+
+    if (removeButton.style.display !== 'none') {
+        removeButton.style.display = 'none';
     }
     else {
-        btnRemove.style.display = 'inline-block';
+        removeButton.style.display = 'inline-block';
     }
 }
 
-function removeButton() {
-    btnAdd.remove();
-    btnRemove.style.display = 'none';
+function removeItem(event) {
+    let removeButton = event.target.parentElement.getElementsByClassName('btn-remove')[0];
+    let removeAll = event.target.parentElement.getElementsByClassName('btn-add')[0];
+    removeAll.remove();
+    removeButton.style.display = 'none';
 }
